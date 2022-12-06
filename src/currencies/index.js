ea25ae820fe63452f398dc1d5eced9d3feb494a0
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-12-06 18:43:09 +0100

    bugs in currencies fixed

--- src/currencies/index.js
+++ src/currencies/index.js
@@ -1,22 +1,21 @@
-const currencies = [
+export const currencies = [
     {
         id: 1,
         rate: 4.70,
-        name: Euro,
-        short: EUR,
+        name: "Euro",
+        short: "EUR",
     },
     {
         id: 2,
         rate: 4.64,
-        name: Dolar,
-        short: USD,
+        name: "Dolar",
+        short: "USD",
     },
     {
         id: 3,
         rate: 5.42,
-        name: Funt,
-        short: GBP,
+        name: "Funt",
+        short: "GBP",
     },
 ];
 
-export default currencies;
