90a56ef4bb23328498e9363bbf929ca785a2b252
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-12-06 18:06:30 +0100

    currencies table added

---
+++ src/currencies/index.js
@@ -0,0 +1,22 @@
+const currencies = [
+    {
+        id: 1,
+        rate: 4.70,
+        name: Euro,
+        short: EUR,
+    },
+    {
+        id: 2,
+        rate: 4.64,
+        name: Dolar,
+        short: USD,
+    },
+    {
+        id: 3,
+        rate: 5.42,
+        name: Funt,
+        short: GBP,
+    },
+];
+
+export default currencies;
