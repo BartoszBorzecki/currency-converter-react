e38eb9841a1b225e0e7cae8c781190591ebd8ae0
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-12-20 17:20:34 +0100

    Clock component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Container from "./Container";
 import Form from "./Form";
+import Clock from "./Clock";
 
 function App() {
 
   return (
     <Container>
+      <Clock />
       <Form />
     </Container>
   );
